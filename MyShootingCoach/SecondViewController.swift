a5bfa183fc5f3735c1955f6d5a63ba99e24beaf3
Satoshi Komatsu <zlia.6.lj.425@gmail.com>
2017-09-23 23:10:48 +0900

    Initial Commit

---
+++ MyShootingCoach/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyShootingCoach
+//
+//  Created by Satoshi Komatsu on 2017/09/23.
+//  Copyright © 2017年 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
