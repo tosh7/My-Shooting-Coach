17f2fd8203696203d65444f36775aceb1b5e0a31
Satoshi Komatsu <zlia.6.lj.425@gmail.com>
2017-10-02 22:05:53 +0900

    10/2　勉強会終了後

--- MyShootingCoach/Classes/File.swift
+++ MyShootingCoach/Classes/File.swift
@@ -11,8 +11,10 @@ import RealmSwift
 
 class ShootData: Object{
     
-    @objc dynamic var day: String = ""
+    @objc dynamic var month: Int = 0
+    @objc dynamic var day: Int = 0
     @objc dynamic var take: Int = 0
     @objc dynamic var make: Int = 0
+    @objc dynamic var percent: Double = 0
     
 }
--- MyShootingCoach/ViewControllers/PostViewController.swift
+++ MyShootingCoach/ViewControllers/PostViewController.swift
@@ -7,7 +7,51 @@
 //
 
 import UIKit
+import RealmSwift
 
 class PostViewController: UIViewController {
-
+    
+    @IBOutlet weak var month: UITextField!
+    @IBOutlet weak var day: UITextField!
+    @IBOutlet weak var make: UITextField!
+    @IBOutlet weak var take: UITextField!
+    
+    var data_make: Double!
+    var data_take: Double!
+    
+    let po = ShootData()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("here")
+        
+    }
+    
+    @IBAction func Post(_ sender: Any) {
+        
+        po.month = Int(month.text!)!
+        po.day = Int(day.text!)!
+        po.take = Int(make.text!)!
+        po.make = Int(take.text!)!
+        
+        data_make = Double(make.text!)!
+        data_take = Double(take.text!)!
+        
+        po.percent = data_make / data_take
+        
+        print("data_make is ", data_make)
+        print("data_take is ", data_take)
+        print("po.percent is ", po.percent)
+        
+        let realm = try! Realm()
+        try! realm.write{
+            realm.add(po)
+        }
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
